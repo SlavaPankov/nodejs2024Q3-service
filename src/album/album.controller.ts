939692107b02011ad6313c7d74b70f6a0b697925
spankov <spankov@ikod.kz>
2024-11-07 09:25:47 +0500

    feat: implement findAll, findOne, create methods for album module

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { AlbumService } from './album.service';
+import { CreateAlbumDto } from './dto/createAlbum.dto';
 
 @Controller('album')
-export class AlbumController {}
+export class AlbumController {
+  constructor(private readonly albumService: AlbumService) {}
+
+  @Get()
+  findAll() {
+    return this.albumService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.albumService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createAlbum: CreateAlbumDto) {
+    return this.albumService.create(createAlbum);
+  }
+}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DbService } from '../db/db.service';
+import { EErrorMessage } from '../types/messages';
+import { CreateAlbumDto } from './dto/createAlbum.dto';
+import { AlbumEntity } from './entity/album.entity';
+import { EDbEntity } from '../types/dbentity';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  constructor(private db: DbService) {}
+
+  async findAll() {
+    return this.db.albums;
+  }
+
+  async findOne(id: string) {
+    const album = this.db.albums.find((album) => album.id === id);
+
+    if (!album) {
+      throw new NotFoundException(EErrorMessage.ALBUM_NOT_FOUND);
+    }
+
+    return album;
+  }
+
+  async create(body: CreateAlbumDto) {
+    if (body.artistId) {
+      const isExistsArtist = this.db.checkEntityExistence(
+        body.artistId,
+        EDbEntity.ARTISTS,
+      );
+      if (!isExistsArtist) {
+        throw new NotFoundException(EErrorMessage.ARTIST_NOT_FOUND);
+      }
+    }
+
+    const createdAlbum = new AlbumEntity(body);
+
+    this.db.albums.push(createdAlbum);
+
+    return createdAlbum;
+  }
+}
