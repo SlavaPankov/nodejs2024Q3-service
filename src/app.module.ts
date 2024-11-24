23357dfaa6e96010726de77303e9daf0fa1c2d10
spankov <spankov@ikod.kz>
2024-11-24 13:18:28 +0500

    feat: implement logging and error handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,33 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { LoggerModule } from './logger/logger.module';
+import { HttpRequestLoggerMiddleware } from './utils/httpRequestLoggerMiddleware';
+import { CoreModule } from './utils/coreModule';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    CoreModule,
     UsersModule,
     TrackModule,
     AlbumModule,
     ArtistModule,
     FavoriteModule,
+    LoggerModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(HttpRequestLoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
